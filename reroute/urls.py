db6a3e9bf5d2fcde32230e180b47b8f0baf191ae
jcoffer921 <jcoffer921@gmail.com>
2025-08-22 13:44:02 -0400

    Fixed profile urls

--- reroute/urls.py
+++ reroute/urls.py
@@ -2,22 +2,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from dashboard.views import dashboard_redirect
-from profiles.views import public_profile_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # App mounts
     path('', include('main.urls')),
     path('jobs/', include('job_list.urls')),
-    path('<str:username>/', public_profile_view, name='public_profile'),
     path('resume/', include(('resumes.urls', 'resumes'), namespace='resumes')),
     path('accounts/', include('allauth.urls')),
     path('dashboard/', include(('dashboard.urls', 'dashboard'), namespace='dashboard')),
-    path("resources/", include("resources.urls")),
-    path('profile/', include('profiles.urls')),
-    path("blog/", include("blog.urls")),
+    path('resources/', include('resources.urls')),
+    path('profile/', include('profiles.urls')),   # <-- keep this
+    path('blog/', include('blog.urls')),
 
-    # ✅ Only one skills-related route needed (API)
+    # API
     path('api/', include('core.urls')),
 ]
 
